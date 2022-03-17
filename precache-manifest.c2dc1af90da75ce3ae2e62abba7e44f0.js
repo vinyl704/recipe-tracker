7cb29de4bbfd851f5a57088c9a987e384272fb3a
vinyl704 <mpbarber@mymail.gaston.edu>
2022-03-17 17:39:58 -0400

    Updates

---
+++ precache-manifest.c2dc1af90da75ce3ae2e62abba7e44f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6015135074417e5a6e9f8c0c02d6249",
+    "url": "/recipe-tracker/index.html"
+  },
+  {
+    "revision": "d3618644a688e1376fcc",
+    "url": "/recipe-tracker/static/css/main.c5cdd117.chunk.css"
+  },
+  {
+    "revision": "f394498dd0083a989c2b",
+    "url": "/recipe-tracker/static/js/2.6d741cec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe-tracker/static/js/2.6d741cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3618644a688e1376fcc",
+    "url": "/recipe-tracker/static/js/main.8cbb298f.chunk.js"
+  },
+  {
+    "revision": "982fa5ef2b198456df9f",
+    "url": "/recipe-tracker/static/js/runtime-main.246dc71b.js"
+  }
+]);
